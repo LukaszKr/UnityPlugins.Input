bbfc499dd7c9b8a83b79a5ff0ddf30b1d0952374
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2020-12-12 13:06:40 +0100

    Fix for forward/backward buttons on mouse being detected incorrectly.

--- Runtime/Enum/EMouseButton.cs
+++ Runtime/Enum/EMouseButton.cs
@@ -1,11 +1,18 @@
-﻿namespace ProceduralLevel.UnityPlugins.Input
+﻿using System;
+
+namespace ProceduralLevel.UnityPlugins.Input
 {
 	public enum EMouseButton
 	{
 		Left = 0,
 		Right = 1,
 		Middle = 2,
-		Back = 4,
-		Forward = 5
+		Back = 3,
+		Forward = 4
+	}
+
+	public static class EMouseButtonExt
+	{
+		public static readonly EMouseButton[] Values = (EMouseButton[])Enum.GetValues(typeof(EMouseButton));
 	}
 }
